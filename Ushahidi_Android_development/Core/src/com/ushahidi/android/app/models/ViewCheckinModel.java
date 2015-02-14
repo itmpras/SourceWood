bb4034014fa2e5d3e05b0b98a7667e789ce2fafa
itmpras <itmpras@gmail.com>
2015-02-14 19:56:56 +0000

    Intial check in

---
+++ Ushahidi_Android_development/Core/src/com/ushahidi/android/app/models/ViewCheckinModel.java
@@ -0,0 +1,5 @@
+package com.ushahidi.android.app.models;
+
+public class ViewCheckinModel extends Model {
+
+}
---
+++ Ushahidi_Android_development/Core/src/com/ushahidi/android/app/models/ViewReportModel.java
@@ -0,0 +1,4 @@
+package com.ushahidi.android.app.models;
+
+public class ViewReportModel extends Model {
+}
