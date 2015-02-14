d8a95851f3913038849f48f4416d8f146d38b9f6
itmpras <itmpras@gmail.com>
2015-02-14 21:29:35 +0000

    Initial commit

---
+++ Ushahidi_Android-develop-/Core/tests/src/com/ushahidi/android/app/ui/phone/test/AddReportActivityTest.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.ushahidi.android.app.ui.phone.test;
+
+
+/**
+ * @author eyedol
+ *
+ */
+public class AddReportActivityTest {
+
+	public AddReportActivityTest() {
+		//TODO:: implement testing for adding a new report
+	}
+
+}
